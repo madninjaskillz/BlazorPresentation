edd780eda1a14ea47ad0157bb821b0a33b16a465
James Johnston <james.johnston@red-gate.com>
2021-05-11 15:24:47 +0100

    Add project files.

---
+++ BlazorPresentation/wwwroot/webcam.js
@@ -0,0 +1,14 @@
+﻿function startVideo() {
+    var video = document.getElementById('video');
+
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+            try {
+                video.srcObject = stream;
+            } catch (error) {
+                video.src = window.URL.createObjectURL(stream);
+            }
+            video.play();
+        });
+    }
+}
